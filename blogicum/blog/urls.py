d8bdd73ce4df9f60e32b2859c3a2b0abe4662694
miien <milegosik@gmail.com>
2025-11-25 04:41:28 +0500

    Update urls.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
-    path('debug/category/<slug:category_slug>/', views.debug_category),  # ← временно
+    path('category/<slug:category_slug>/', views.category_posts,
+         name='category_posts'),
+    path('debug/category/<slug:category_slug>/', views.debug_category),
 ]
