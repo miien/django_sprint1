d4c4d9e9397819db240fcd8fea8a0a54ae945c79
miien <milegosik@gmail.com>
2025-11-25 03:29:02 +0500

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -47,7 +47,7 @@ posts = [
 
 def index(request):
     context = {'posts': posts}
-    return render(request, 'index.html', context)
+    return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, id):
@@ -59,9 +59,9 @@ def post_detail(request, id):
     if post is None:
         post = {}
     context = {'post': post}
-    return render(request, 'detail.html', context)
+    return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
     context = {'category_slug': category_slug}
-    return render(request, 'category.html', context)
+    return render(request, 'blog/category.html', context)
